3708e725a3d37ce01618e428c008536a7c61b17e
Vasa <mail@vasa.me>
2025-08-20 10:38:18 +0200

    feat: add deployment scripts for GitHub Pages

---
+++ script/script/deploy-to-gh-pages.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+cd dist || exit
+
+echo 'chapter-39.vasa.me' > CNAME
+echo 'Go to [chapter-39.vasa.me](https://chapter-39.vasa.me)' > README.md
+
+git config --global user.email "chapter-39@vasa.me "
+git config --global user.name "Chapter-39"
+
+git init
+git checkout --orphan gh-pages
+git add .
+git commit -m "Deploy to GitHub Pages"
+git remote add origin https://"${GITHUB_ACTOR}":"${NODE_AUTH_TOKEN}"@github.com/"${GITHUB_REPOSITORY}".git
+git push origin gh-pages --force
+
+# git branch -M gh-pages
+# git remote add origin https://github.com/vasagrujic/eaten.fit.git
+# git push -u origin gh-pages
+
+exit
